d7a8b714bc8f4439a8d275e1c9d393163c967b77
Giovana <giovanadutramaga@gmail.com>
2025-02-05 08:52:39 -0300

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿class Program{
+
+    static void Main(string[] args){
+
+        //Exercício 01
+
+        /*System.Console.WriteLine("Insira um número: ");
+        var numero1 = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Insira o segundo número: ");
+        var numero2 = int.Parse(Console.ReadLine());
+
+        var soma = numero1 + numero2;
+        var sub = numero1 - numero2;
+        var mult = numero1 * numero2;
+        double div = numero1 / numero2;
+
+        System.Console.WriteLine($"Soma: {soma}");
+        System.Console.WriteLine($"Subtração: {sub}");
+        System.Console.WriteLine($"Multiplicação {mult}");
+        System.Console.WriteLine($"Divisão: {div}"); */
+
+        //Exercício 02
+
+        /*System.Console.WriteLine("Insira uma valor: ");
+        float valor = float.Parse(Console.ReadLine());
+
+        double desconto = valor - (valor * 0.1);
+
+        System.Console.WriteLine($"O desconto é de {desconto}");*/
+
+        //Exercício 03
+
+        /*for(int numero3 = 0; numero3 < 21; numero3 ++ ){
+
+            if(numero3 % 2 == 0){
+
+                System.Console.WriteLine($"{numero3}");
+
+            }
+
+        }*/
+
+        //Exercício 04
+
+        /*System.Console.WriteLine("Insira um número: ");
+        var numero4 = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Insira o segundo número: ");
+        var numero5 = int.Parse(Console.ReadLine());
+
+        int soma1 = 0;
+
+        for(int i = numero4; i <= numero5; i ++ ){
+
+            soma1 += i;
+
+        }
+
+        System.Console.WriteLine($"A soma dos números no intervalo do número {numero4} e número {numero5} é {soma1}");*/
+
+        //Exercício 05
+
+        System.Console.WriteLine("Insira o 1° número: ")
+        var numero6 = int.Parse
+    }
+}
